32e46f3c7f15994be4b22936b5d9c924f95b45e5
Esteban Leon <eleon182@gmail.com>
2015-10-18 10:57:12 -0700

    update

---
+++ src/main/java/dow/data/UserProfileDAta.java
@@ -0,0 +1,12 @@
+package dow.data;
+
+import dow.data.dao.UserProfileDAO;
+
+/**
+ * Created by steve on 10/18/15.
+ */
+public class UserProfileData extends AWSDatabase {
+    public UserProfileDAO getUserData(String user){
+        return getMapper().load(UserProfileDAO.class, user);
+    }
+}
